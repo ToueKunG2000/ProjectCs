2109f80df6f6dd2c1b663ca52daa45e9710d572a
ToueKunG2000 <tanapon.srin@ku.th>
2023-01-03 14:22:32 +0700

    Initial Frontend Project

---
+++ frontend-project/pages/index.tsx
@@ -0,0 +1,13 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+
+
+export default function Home() {
+  return (
+    <>
+      <h1>Login Page</h1>
+      <h2>Hello</h2>
+    </>
+  )
+}
