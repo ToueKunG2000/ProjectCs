e5aade75016868528deccb4f6af8adc1ee9463ed
ToueKunG2000 <tanapon.srin@ku.th>
2023-01-03 15:27:51 +0700

    modify layout in login and add primeflex

--- frontend-project/component/login/template.tsx
+++ frontend-project/component/login/template.tsx
@@ -1,19 +1,23 @@
 import { InputText } from 'primereact/inputtext';
 import { Button } from 'primereact/button';
-import { Card } from 'primereact/card';
 
 const LayoutLogin = () => {
     return (
-        <>
-            <Card>
-                <label>Username : </label>
-                <InputText />
-                <label>Password : </label>
-                <InputText />
-                <Button 
-                    label='Login'
-                />
-            </Card>
+        <>  
+        <div className="flex flex-column justify-content-center">
+            <div className='flex justify-content-center'>
+            <label>Username : </label>
+            <InputText />   
+            </div>
+            <div className='flex justify-content-center'>
+            <label>Password : </label>
+            <InputText />
+            </div>
+            <Button 
+                label='Login'
+                className="p-button-info"
+            />
+        </div>
         </>
     );
 }
--- frontend-project/pages/_app.tsx
+++ frontend-project/pages/_app.tsx
@@ -1,4 +1,8 @@
 import type { AppProps } from 'next/app'
+import "primereact/resources/themes/lara-light-indigo/theme.css";  //theme
+import "primereact/resources/primereact.min.css";                  //core css
+import "primeicons/primeicons.css";                                //icons
+import "/node_modules/primeflex/primeflex.css";
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
